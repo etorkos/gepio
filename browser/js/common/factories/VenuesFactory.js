80b045cc5ef14ffee31c2d61aa46c247a69b5aac
Jan Bialostok <janbialostok@gmail.com>
2015-03-17 17:19:07 -0400

    Inital yelp query

--- browser/js/common/factories/VenuesFactory.js
+++ browser/js/common/factories/VenuesFactory.js
@@ -8,6 +8,21 @@ app.factory('VenuesFactory', function($http){
 				console.log('response from server', response.data);
 				return response.data;
 			})
+		},
+		getLocationsYelp: function(data){
+			console.log('sending request to Yelp');
+			var consumerKey = "dJs9nnzSGymTp3sNWgOBwg";
+			var consumerSecret = "PHKlEhNoQnMVKUqlqkgYrHju-24";
+			var token = "K5YvukEBKvpov4KPKKNFkBrSNILaO_8n";
+			var tokenSecret = "dzKKvUE3WQjqb4aEIWT3Zza38vk";
+			var timestamp = Date.now() / 1000;
+			var str = "abc123";
+			var url = 'http://api.yelp.com/vs/search?term=food&location=New+York&oauth_comsumer_key=' + consumerKey + '&oauth_token=' + token + '&oauth_signature_method=hmac-sha1&oauth_signature=' + tokenSecret + '&oauth_timestamp=' + timestamp + '&oauth_nonce=' + str;
+			return $http.get(url).then(function (res){
+				console.log("Yelp Data", res.data);
+				return res.data;
+			});
+
 		}
 	}
 

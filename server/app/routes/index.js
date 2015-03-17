a2f3d7f6c3c94f092d78d2da5cd8e232888eedd1
Jan Bialostok <janbialostok@gmail.com>
2015-03-17 18:53:06 -0400

    Yelp search working

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,5 +1,19 @@
 'use strict';
 var router = require('express').Router();
+var yelp = require('yelp');
 module.exports = router;
 
 router.use('/tutorial', require('./tutorial'));
+
+var yelp_client = yelp.createClient({
+	"consumer_key": "dJs9nnzSGymTp3sNWgOBwg",
+	"consumer_secret": "PHKlEhNoQnMVKUqlqkgYrHju-24",
+	"token": "K5YvukEBKvpov4KPKKNFkBrSNILaO_8n",
+	"token_secret": "dzKKvUE3WQjqb4aEIWT3Zza38vk"
+});
+
+router.post('/yelp/search', function (req, res){
+	yelp_client.search({term: "Fraunces", location: "New York"}, function (err, data){
+		res.json(data);
+	});
+});

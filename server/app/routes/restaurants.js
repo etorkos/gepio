a35c0e7ae279d098974364e3432c58fdf7ecc3b7
Jan Bialostok <janbialostok@gmail.com>
2015-03-18 11:48:53 -0400

    After merge commit

--- server/app/routes/restaurants.js
+++ server/app/routes/restaurants.js
@@ -11,6 +11,13 @@ var yelp_client = yelp.createClient({
 	"token_secret": "dzKKvUE3WQjqb4aEIWT3Zza38vk"
 });
 
+var foursquare_client = {
+
+};
+var foursquareId = 0RJHOSIA5K2RGK3J3SPOYNMET0HFOLDI0SGMIGQ1YVO0JSMJ;
+var foursquareClientSecret=1POC3EVMA3WZ3S01X0AINBTTVY0VHALFN0IVD0PYHCG1AW1M;
+var date = 20150317;
+
 router.post('/yelp/search', function (req, res){
 	yelp_client.search({term: "Fraunces", location: "New York"}, function (err, data){
 		res.json(data);
@@ -34,6 +41,11 @@ router.post('/opentable/search',function (req, res, next){
 	.on('error',function (e){
 		console.log("error: " + e.message)
 	});
+});
+
+router.post('/restaurants/search', function (req, res, next){
+	var city = req.body.city;
+
 });
 
 module.exports = router;

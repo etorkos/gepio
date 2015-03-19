2658b127b35f2be386e4d554053a851f41b90daf
Evan Torkos <etorkos@gmail.com>
2015-03-19 17:37:22 -0400

    buttons looking decent, fixing oauth

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -10,13 +10,31 @@ app.directive('navbar', function () {
     };
 });
 
-app.controller('NavbarCtrl', function($scope, $state){
+app.controller('NavbarCtrl', function($scope, $state, AuthService){
 
 
 	$scope.loggedIn = false; //will need to make a function dependent on session
+    $scope.loginClicked = false;
 
+    AuthService.getLoggedInUser().then(function(user){
+        if(user){
+            $scope.loggedIn = true;
+        }
+        else {
+            $scope.loggedIn = false;
+        }
+    })
+    $scope.logOut = function(){
+        AuthService.logout();
+        $window.location.reload();
+    }
 
-	$scope.toggleVisible = function(){
-		$state.go('navbar.login');
-	}
+    $scope.toggleVisible = function(){
+        $scope.loginClicked = !$scope.loginClicked;
+    }
+
+    $scope.redirect = function(){
+        $state.go('home');
+    }
+
 })

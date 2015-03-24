fc2f8d4d472d6ebec8998474526d7edec069b6ab
Eric Gonzalo <ersgonzalo@gmail.com>
2015-03-24 15:01:12 -0400

    Start on indexing

--- browser/js/common/directives/list-ranking/list-ranking.js
+++ browser/js/common/directives/list-ranking/list-ranking.js
@@ -7,13 +7,24 @@ app.directive('listRanking', function(){
 		},
 		controller: function($scope){
 			
+			var checkRanking = function(item){
+				if(!item.hasOwnProperty('ranking'))
+					item.ranking = 0;
+			};
+			
 			$scope.downvote = function(item){
-
+				checkRanking(item)				
+				item.ranking--;
+				console.log(item.ranking);	
 			};
 
 			$scope.upvote = function(item){
+				checkRanking(item)
+				item.ranking++;
+				console.log(item.ranking);
 
 			};
+
 		},
 		templateUrl: 'js/common/directives/list-ranking/list-ranking.html'
 	};

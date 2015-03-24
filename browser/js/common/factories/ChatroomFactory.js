e5fd311c5fdae86b4b67fc94573d2f8b53a76901
bosonX <xjin1@binghamton.edu>
2015-03-24 17:06:54 -0400

    join room works

--- browser/js/common/factories/ChatroomFactory.js
+++ browser/js/common/factories/ChatroomFactory.js
@@ -3,12 +3,10 @@ app.factory('ChatroomFactory', function (){
 	var current_nsp;
 	return {
 		send_message_to_server : function(message_text){
-			// alert(current_nsp)
-			if(current_nsp){
-				socket.emit('message',{
-					message : message_text
-				});
-			}
+			socket.emit('message',{
+				// name : user.firstName,
+				message : message_text
+			});
 		},
 		create_room : function(room_name){
 			socket.emit('create_room',{
@@ -19,8 +17,7 @@ app.factory('ChatroomFactory', function (){
 			console.log(socket)
 		},
 		join_room : function(room_name){
-			current_nsp = '/' + room_name;
-			socket = io(current_nsp);
+			socket.emit('join_room',room_name)
 		},
 		open_invitation : function(location,range,room_name){
 			socket.emit('open_invitation',{
@@ -29,9 +26,7 @@ app.factory('ChatroomFactory', function (){
 			})
 		},
 		leave_room : function(){
-			socket.emit('leave_room',{
-
-			})
+			socket.emit('leave_room');
 		},
 		up_vote: function(){
 			// socket.emit('')
--- server/io/index.js
+++ server/io/index.js
@@ -10,42 +10,38 @@ module.exports = function (server) {
     io = socketio(server);
 
 	io.on('connection',function(client){
-		// function nsp_communication(client){
-		// 	var current_nsp = io.of(client.nsp);
-
-		// 	io.of(current_nsp).on('new_message',function(data){
-		// 		// io.of(current_nsp).
-		// 	});
-		// }
 		console.log("client connected");
 
 		client.on('location',function(data){
 			client.geo_location = data;
 		});
 
+
+		// client.on('create_room',function(data){
+		// 	// console.log(data);
+		// 	var nsp = io.of('/'+data.room_name)
+		// 	namespaces.push(nsp);
+		// 	// console.log(namespaces);
+		// 	  nsp.on('message',function(thing){
+		// 	  	console.log(thing, "from nsp");
+		// 	  });
+		// 	nsp.on('connection', function(client_in_nsp){
+		// 	  console.log('someone connected to' + data.room_name);
+
+		// 	});
+		// 	nsp.emit('hi', 'everyone!');
+		// });
+
+		client.on('join_room',function(room_name){
+			client.room = room_name;
+			client.join(client.room);
+		});
+
 		client.on('message',function(data){
-			console.log(data);
-			client.broadcast.emit('new_message',data);
+			if(client.room){
+				client.broadcast.to(client.room).emit('new_message',data);
+			}
 		});
-
-		client.on('create_room',function(data){
-			// console.log(data);
-			var nsp = io.of('/'+data.room_name)
-			namespaces.push(nsp);
-			// console.log(namespaces);
-			  nsp.on('message',function(thing){
-			  	console.log(thing, "from nsp");
-			  });
-			nsp.on('connection', function(client_in_nsp){
-			  console.log('someone connected to' + data.room_name);
-
-			});
-			nsp.emit('hi', 'everyone!');
-		});
-
-		client.on('join_room',function(data){
-			client.join(data.name);
-		})
 
 		client.on('open_invitation',function(data){
 			client.emit('open_invitation',{

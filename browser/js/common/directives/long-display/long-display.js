72ebb535ffce25257218e4ddc20b7cdb4184fa0f
Eric Gonzalo <ersgonzalo@gmail.com>
2015-03-26 15:55:29 -0400

    Quick fixes and start working on planning

--- browser/js/common/directives/long-display/long-display.js
+++ browser/js/common/directives/long-display/long-display.js
@@ -4,9 +4,7 @@ app.directive('longDisplay', function(){
 		restict: 'E',
 		templateUrl: 'js/common/directives/long-display/long-display.html',
 		link: function(scope, el, attrs){
-			// var displayParams = ['events', 'venues'];
-			// if(arrPrm.indexOf('events') !== -1 && arrPrm.indexOf('venues'))
-			scope.eventType = 'Restaurants';
+			scope.eventThing = attrs.eventType;
 		}
 	};
 });
